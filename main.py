02956054b8fea8ccb93712c31f506a75bad474d7
mann01 <39325832+mann01@users.noreply.github.com>
2021-10-29 19:10:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ pdfFileObj = open('edit2strip.pdf', 'rb')
 pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
 
 print(pdfReader.numPages)
-
+#variables modified
 pageObj = pdfReader.getPage(0)
 print(pageObj.extractText())
 
